927941565754bbe0793a9ee93ad1df06294d08b8
itays77 <itaysoldin8@gmail.com>
2025-01-08 19:01:13 +0200

    StartUserProfileForm

--- backend/src/middleware/validation.ts
+++ backend/src/middleware/validation.ts
@@ -1,5 +1,20 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestHandler } from 'express';
+
+const handleValidationErrors: RequestHandler = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+  next();
+};
 
 export const validateMyUserRequest = [
-  body("name").isString().notEmpty().withMessage("Name is required"),
+  body('name').isString().notEmpty().withMessage('Name is required'),
+  body('adressLine1').isString().notEmpty().withMessage('Address is required'),
+  body('city').isString().notEmpty().withMessage('City is required'),
+  body('country').isString().notEmpty().withMessage('Country is required'),
+  handleValidationErrors,
 ];
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,17 @@
 import express, { RequestHandler } from 'express';
 import MyUserController from '../controllers/MyUserController';
 import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
 router.post('/', jwtCheck, MyUserController.createCurrentUser as RequestHandler);
-router.put('/', jwtCheck, jwtParse,  MyUserController.updateCurrentUser as RequestHandler);
+router.put(
+  '/',
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser as RequestHandler
+);
 
 export default router;
